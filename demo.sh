f9946b5a84ba06ddd5786fd74c012fc5ab43803b
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2025-04-15 11:16:55 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -5,3 +5,4 @@ aws_s3_secret_key = s3_raw_secret_key
 AWS_ACCESS_KEY="JlZC3i8nCmzdL7lQZQ"
 <name>richTextValue</name>
 <value>&lt;p&gt;&lt;strong style=&quot;font-size: 16px;&quot;&gt;Recent Enhancements:&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;ol&gt;&lt;li&gt;&lt;a href=&quot;https://sites.google.com/infoblox.com/onboarding-am/manage-my-opportunities?authuser=0#h.w02ohfypfeu6&quot; rel=&quot;noopener noreferrer&quot; target=&quot;_blank&quot;&gt;BDR to Sales handoff process&lt;/a&gt;&lt;/li&gt;&lt;li&gt;Account Owner ability to edit all contacts un
+token = "P$5aRt#2oN!9lM@8as"
